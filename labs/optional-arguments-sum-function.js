3c4c48f3779201e689867174673f9b971df1f58b
topinii <niiranentopi@gmail.com>
2025-10-06 11:24:52 +0300

    optional arguments sum function

---
+++ labs/optional-arguments-sum-function.js
@@ -0,0 +1,34 @@
+function addTogether(first, second) {
+    // Helper function to check if a value is a valid number
+    function isValidNumber(num) {
+        return typeof num === 'number' && !isNaN(num);
+    }
+    
+    // If two arguments are provided
+    if (arguments.length === 2) {
+        // Check if both arguments are valid numbers
+        if (isValidNumber(first) && isValidNumber(second)) {
+            return first + second;
+        } else {
+            return undefined;
+        }
+    }
+    
+    // If only one argument is provided
+    if (arguments.length === 1) {
+        // Check if the first argument is a valid number
+        if (!isValidNumber(first)) {
+            return undefined;
+        }
+        
+        // Return a function that waits for the second argument
+        return function(second) {
+            // Check if the second argument is a valid number
+            if (isValidNumber(second)) {
+                return first + second;
+            } else {
+                return undefined;
+            }
+        };
+    }
+}
