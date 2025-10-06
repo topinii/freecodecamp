c16b2dff776177b80a1241e1b823e39583b7ed40
topinii <niiranentopi@gmail.com>
2025-10-06 11:16:57 +0300

    html entity converter

---
+++ labs/html-entity-converter.js
@@ -0,0 +1,16 @@
+function convertHTML(str) {
+    // Create an object mapping characters to their HTML entities
+    const htmlEntities = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': "&quot;",
+        "'": "&apos;"
+    };
+    
+    // Accepts a string as input
+    // Uses a regex pattern /[&<>"']/g to find all instances of the special characters globally
+    // </>Replaces each character with its corresponding HTML entity using the htmlEntities object
+    // Returns the converted string
+    return str.replace(/[&<>"']/g, char => htmlEntities[char]);
+}
