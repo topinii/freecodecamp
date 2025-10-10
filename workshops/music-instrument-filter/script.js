72a489857e887bd4f59122e21621702e60463a3d
topinii <niiranentopi@gmail.com>
2025-10-10 10:41:02 +0300

    music instrument filter init

---
+++ workshops/music-instrument-filter/script.js
@@ -0,0 +1,16 @@
+const instrumentsArr = [
+    { category: "woodwinds", instrument: "Flute", price: 500 },
+    { category: "woodwinds", instrument: "Clarinet", price: 200 },
+    { category: "woodwinds", instrument: "Oboe", price: 4000 },
+    { category: "brass", instrument: "Trumpet", price: 200 },
+    { category: "brass", instrument: "Trombone", price: 300 },
+    { category: "brass", instrument: "French Horn", price: 4300 },
+    { category: "percussion", instrument: "Drum Set", price: 500 },
+    { category: "percussion", instrument: "Xylophone", price: 3000 },
+    { category: "percussion", instrument: "Cymbals", price: 200 },
+    { category: "percussion", instrument: "Marimba", price: 3000 }
+  ]
+  
+  const selectContainer = document.querySelector("select");
+  const productsContainer = document.querySelector(".products-container");
+  
