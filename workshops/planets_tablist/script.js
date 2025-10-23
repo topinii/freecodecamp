b1e4502df843b1cafe50ce25edbcbf7a1453d440
topinii <niiranentopi@gmail.com>
2025-10-23 13:46:25 +0300

    add script.js

---
+++ workshops/planets_tablist/script.js
@@ -0,0 +1,5 @@
+// Select all elements with role="tab"
+const tabs = document.querySelectorAll('[role="tab"]');
+
+// Select all elements with role="tabpanel"
+const panels = document.querySelectorAll('[role="tabpanel"]');
