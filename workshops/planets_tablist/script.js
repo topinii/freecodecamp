b3e536423d54c1dd9e7799e5bab84ea3800d3659
topinii <niiranentopi@gmail.com>
2025-10-23 13:58:13 +0300

    planets finished

--- workshops/planets_tablist/script.js
+++ workshops/planets_tablist/script.js
@@ -1,11 +1,14 @@
-// Select all elements with role="tab"
 const tabs = document.querySelectorAll('[role="tab"]');
-
-// Select all elements with role="tabpanel"
 const panels = document.querySelectorAll('[role="tabpanel"]');
 
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
+    tabs.forEach(t => t.setAttribute('aria-selected', 'false'));
+    panels.forEach(p => p.hidden = true);
 
+    tab.setAttribute('aria-selected', 'true');
+    const associatedPanel = tab.getAttribute('aria-controls');
+    const panel = document.getElementById(associatedPanel);
+    panel.hidden = false;
   });
 });
