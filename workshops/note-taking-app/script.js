9dc9bfcb5fef48a46a7ca595747d62dec06834ed
topinii <niiranentopi@gmail.com>
2025-10-29 09:02:43 +0200

    note takign app

--- workshops/note-taking-app/script.js
+++ workshops/note-taking-app/script.js
@@ -3,6 +3,10 @@ const statusEl = document.getElementById('status');
 
 let currentContent = '';
 
+noteEl.addEventListener('blur', () => {
+  const newContent = noteEl.innerHTML;
+});
+
 window.addEventListener('DOMContentLoaded', () => {
   noteEl.textContent = currentContent;
 });
