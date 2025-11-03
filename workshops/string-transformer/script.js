d722842a8d0fafb6354c06236187be65bb49273b
topinii <niiranentopi@gmail.com>
2025-11-04 00:06:06 +0200

    string transformer

--- workshops/string-transformer/script.js
+++ workshops/string-transformer/script.js
@@ -9,3 +9,7 @@ console.log(replacedString);
 const exampleSentence = 'I love cats and cats are so much fun!';
 console.log('Original sentence:');
 console.log(exampleSentence);
+
+const dogsOnlySentence = exampleSentence.replace('cats', 'dogs');
+console.log('Replacing all occurrences of cats with dogs:');
+console.log(dogsOnlySentence);
