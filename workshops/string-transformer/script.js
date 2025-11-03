0a5acd3271056f931e03b24f6281c30887da7b77
topinii <niiranentopi@gmail.com>
2025-11-04 00:04:03 +0200

    string transformer

--- workshops/string-transformer/script.js
+++ workshops/string-transformer/script.js
@@ -1,3 +1,7 @@
 const originalString = 'I love cats.';
 console.log('Original string:');
 console.log(originalString);
+
+const replacedString = originalString.replace('cats', 'dogs');
+console.log('After using the replace() method:');
+console.log(replacedString);
